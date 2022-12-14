b00f819678ba3e1e4a017f412f5a77f97ba9ae09
Лариса Лариса <larisalarisa@MacBook-Air-Larisa.local>
2022-12-14 03:50:27 +0200

    Starting

---
+++ src/main/java/com/askerproject/asker/AskerApplication.java
@@ -0,0 +1,13 @@
+package com.askerproject.asker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/askerproject/asker/AskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askerproject.asker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
